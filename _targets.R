126ccf8591f44b54c0b037405698d7945a5ad462
Ryan Peek <rapeek@ucdavis.edu>
2022-11-09 11:11:58 -0800

    update code base

--- _targets.R
+++ _targets.R
@@ -17,14 +17,21 @@ tar_source("code/")
 
 # list the steps
 list(
-  # rule to download hubs
+  # rule to download USDA Climate Hubs:
+  ## California = 1, Caribbean = 2, Midwest = 3, Northeast = 4, Northern Forests = 5, Northern Plains = 6, Northwest = 7, Southeast  = 8, Southern Plains = 9, Southwest = 10
   tar_target(download_hub,
              command = f_get_dm_data(aoi = c(1,7,10))),
+  tar_target(download_hub_dsci,
+             command = f_get_dm_data(
+               aoi = c(1,7,10),
+               stats_type="GetDSCI",
+               id_out = "dsci_west")),
 
   # rule to download counties
   tar_target(cnty_fips, f_get_counties() %>%
                filter(county %in%
                         c("Yolo", "Stanislaus", "Tulare", "Kern"))),
+  # download county data
   tar_target(download_cnty,
              command = f_get_dm_data(
                area = "CountyStatistics",
@@ -43,15 +50,19 @@ list(
 
   # load the local data
   tar_target(read_hub, f_load_local(download_hub)),
+  tar_target(read_hub_dsci, f_load_local(download_hub_dsci)),
   tar_target(read_cnty, f_load_local(download_cnty)),
   tar_target(read_hucs, f_load_local(download_hucs)),
 
   # clean data
   tar_target(clean_dat_hub, f_clean_data_hub(read_hub)),
   tar_target(clean_dat_cnty, f_clean_data_cnty(read_cnty)),
+  tar_target(clean_dat_hucs, f_clean_data_hucs(read_hucs)),
+  tar_target(clean_dat_hub_dsci, f_clean_data_hub_dsci(read_hub_dsci)),
 
   # make plot
   tar_target(make_plot_hub, f_make_barplot_hub(clean_dat_hub, 2010)),
   tar_target(make_plot_cnty, f_make_barplot_cnty(clean_dat_cnty))
+  #tar_target(make_plot_hucs, f_make_barplot_cnty(clean_dat_huc, "Gualala-Salmon"))
 )
 
