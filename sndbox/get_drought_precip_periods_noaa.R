d2c06869e53098a7b6a634dbdccc64f2aaf37f9e
Ryan Peek <rapeek@ucdavis.edu>
2022-11-15 17:14:56 -0800

    update precip

---
+++ sndbox/get_drought_precip_periods_noaa.R
@@ -0,0 +1,40 @@
+# get precip data from noaa
+
+library(httr)
+library(glue)
+library(fs)
+library(dplyr)
+library(readr)
+library(purrr)
+library(lubridate)
+library(tigris)
+
+
+# Path --------------------------------------------------------------------
+
+# https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time-series/CA-017/pcp/36/10/1895-2022.csv?filter=true&filterType=loess
+
+# list of counties
+cntys <- list_counties("CA")
+
+county_sel <- c("El Dorado", "Placer", "Yolo")
+cnty <- filter(cntys, county %in% county_sel) %>%
+  pull(county_code)
+
+# years
+yr_start <- 1895
+yr_end <- 2022
+
+# month
+mon <- 08
+
+# interval: 1-12 mon, 18,24, 36, 48, 60
+interval <- 48
+
+# build url for county CA
+make_url <- glue("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time-series/CA-{cnty}/pcp/{interval}/{mon}/{yr_start}-{yr_end}?base_prd=true&begbaseyear={yr_start}&endbaseyear={yr_end}&filter=true&filterType=binomial")
+
+# use url
+tst <- read_csv(make_url)
+
+
