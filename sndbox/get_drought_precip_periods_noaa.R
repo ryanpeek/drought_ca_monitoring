ad00d7da463d4abbd8d138c788ffb3c7439d4098
Ryan Peek <rapeek@ucdavis.edu>
2022-11-15 17:36:22 -0800

    update

--- sndbox/get_drought_precip_periods_noaa.R
+++ sndbox/get_drought_precip_periods_noaa.R
@@ -17,7 +17,7 @@ library(tigris)
 # list of counties
 cntys <- list_counties("CA")
 
-county_sel <- c("El Dorado", "Placer", "Yolo")
+county_sel <- c("El Dorado", "Kern")
 cnty <- filter(cntys, county %in% county_sel) %>%
   pull(county_code)
 
@@ -32,9 +32,13 @@ mon <- 08
 interval <- 48
 
 # build url for county CA
-make_url <- glue("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time-series/CA-{cnty}/pcp/{interval}/{mon}/{yr_start}-{yr_end}?base_prd=true&begbaseyear={yr_start}&endbaseyear={yr_end}&filter=true&filterType=binomial")
+make_url <- glue("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time-series/CA-{cnty}/pcp/{interval}/{mon}/{yr_start}-{yr_end}.csv?base_prd=true&begbaseyear={yr_start}&endbaseyear={yr_end}&filter=true&filterType=binomial")
 
 # use url
-tst <- read_csv(make_url)
-
+#dat_path <- GET(url=make_url)
+dat_out <- read_csv(make_url, skip = 4, id = "path")
+dat_out <- dat_out %>%
+  mutate(county_code = substr(gsub("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time-series/", "", path), 4,6)) %>%
+  select(-path) %>%
+  left_join(cntys)
 
